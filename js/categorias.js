6c0c6bf32df386009eca655045dac3f66b374720
miSitioWeb1 <cabrerabel@hotmail.com>
2019-02-25 09:24:33 -0600

    menu de categorias creados, solo el machote

---
+++ js/categorias.js
@@ -0,0 +1,20 @@
+function principal(){
+    document.getElementById('inicio').style.borderBottom="5px solid rgb(245, 168, 2)";
+    document.getElementById('ninios').style.borderBottom="5px solid #015865"; 
+    document.getElementById('terror').style.borderBottom="5px solid #015865"; 
+    
+
+}
+
+function infantiles(){
+    document.getElementById('ninios').style.borderBottom="5px solid rgb(245, 168, 2)";
+    document.getElementById('inicio').style.borderBottom="5px solid #015865"; 
+    document.getElementById('terror').style.borderBottom="5px solid #015865"; 
+}
+
+function terror(){
+    document.getElementById('terror').style.borderBottom="5px solid rgb(245, 168, 2)";
+    document.getElementById('ninios').style.borderBottom="5px solid #015865"; 
+    document.getElementById('inicio').style.borderBottom="5px solid #015865"; 
+}
+
