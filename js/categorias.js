1613960f96805cb08261f6896e880a4a6be5b80c
miSitioWeb1 <cabrerabel@hotmail.com>
2019-02-25 10:08:13 -0600

    menu de categorias creados, solo el machote,mejorado

--- js/categorias.js
+++ js/categorias.js
@@ -1,20 +1,35 @@
 function principal(){
     document.getElementById('inicio').style.borderBottom="5px solid rgb(245, 168, 2)";
+    document.getElementById('inicio').style.backgroundColor="#00505C";
+
     document.getElementById('ninios').style.borderBottom="5px solid #015865"; 
+    document.getElementById('ninios').style.backgroundColor="#015865";
+
     document.getElementById('terror').style.borderBottom="5px solid #015865"; 
+    document.getElementById('terror').style.backgroundColor="#015865";
     
 
 }
 
 function infantiles(){
     document.getElementById('ninios').style.borderBottom="5px solid rgb(245, 168, 2)";
+    document.getElementById('ninios').style.backgroundColor="#00505C";
+
+    document.getElementById('inicio').style.backgroundColor="#015865";
     document.getElementById('inicio').style.borderBottom="5px solid #015865"; 
+
     document.getElementById('terror').style.borderBottom="5px solid #015865"; 
+    document.getElementById('terror').style.backgroundColor="#015865";
 }
 
 function terror(){
     document.getElementById('terror').style.borderBottom="5px solid rgb(245, 168, 2)";
+    document.getElementById('terror').style.backgroundColor="#00505C";
+
     document.getElementById('ninios').style.borderBottom="5px solid #015865"; 
+    document.getElementById('ninios').style.backgroundColor="#015865";
+
     document.getElementById('inicio').style.borderBottom="5px solid #015865"; 
+    document.getElementById('inicio').style.backgroundColor="#015865";
 }
 
